4c1e09e4adaf23586b24db1fa4b2a9dc6829a5c3
POUND-LK <gimshandilisha2@gmail.com>
2025-08-17 20:51:26 +0530

    Create session.js

---
+++ session.js
@@ -0,0 +1,13 @@
+//======================== SESSION ID ==============
+
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "put your session id",
+PORT: process.env.PORT || "8000"
+};
